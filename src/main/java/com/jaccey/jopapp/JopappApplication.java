a9272acc730af325b9f4bf040dffdbecea7c2f52
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-10-25 13:59:18 +0100

    project setup

---
+++ src/main/java/com/jaccey/jopapp/JopappApplication.java
@@ -0,0 +1,13 @@
+package com.jaccey.jopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JopappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaccey/jopapp/JopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaccey.jopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
